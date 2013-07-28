8437f0f8dda2b1bbbf90fd299be6508fb0badce7
Tommy Alex <iptux7@gmail.com>
2013-07-28 13:25:30 +0800

    msm8660-common: add TARGET_CPU_VARIANT for cm-10.2
    Change-Id: I766c608f36fb1019f17583a78a032d2c69516e3c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,6 +29,7 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 # Architecture
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a9
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_SMP := true
