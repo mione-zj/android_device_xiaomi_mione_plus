8fb9f0310be722f34bff957cdd281b829ce7b2c0
Matt Filetto <matt.filetto@gmail.com>
2013-09-07 09:08:20 -0700

    msm8660.mk: add some missing audio HALs and bluetooth permissions
    Change-Id: I92c55a312f151cc6e4fc6aba11b2f0254aabba6d
    
    Conflicts:
    
    	msm8660.mk
    
    Signed-off-by: Tommy Alex <iptux7@gmail.com>

--- msm8660.mk
+++ msm8660.mk
@@ -25,6 +25,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
@@ -40,6 +41,8 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio_policy.msm8660 \
     audio.primary.msm8660 \
+    audio.usb.default \
+    libaudio-resampler \
     libaudioutils
 
 # Graphics
