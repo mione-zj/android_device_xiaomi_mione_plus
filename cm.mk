5d34000983ba886598a81f6e54b2c34d5cbfe142
BambooIV <ningdisheng@gmail.com>
2015-04-26 12:13:34 +0800

    mione_plus: miui-v5 bringup

--- cm.mk
+++ cm.mk
@@ -2,13 +2,12 @@
 # Release name
 PRODUCT_RELEASE_NAME := mione_plus
 
-TARGET_BOOTANIMATION_NAME := vertical-480x854
+# Boot animation
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-PRODUCT_PACKAGES += \
-    Firewall \
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/mione_plus/device_mione_plus.mk)
@@ -20,11 +19,11 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI-ONE Plus
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_CHARACTERISTICS := phone
+# CM_BUILDTYPE
+CM_BUILDTYPE := NIGHTLY
 
 # Set build fingerprint / ID / Product Name etc.
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRODUCT_NAME=mione_plus \
-	BUILD_FINGERPRINT="Xiaomi/mione_plus/mione_plus:4.1.2/JZO54K/QDS84:userdebug/test-keys" \
-	PRIVATE_BUILD_DESC="mione_plus-userdebug 4.1.2 JZO54K QDS84 test-keys" \
-
+	BUILD_FINGERPRINT="Xiaomi/mione_plus/mione_plus:4.1.2/JZO54K/4.12.5:user/release-keys" \
+	PRIVATE_BUILD_DESC="mione_plus-user 4.1.2 JZO54K 4.12.5 release-keys"
