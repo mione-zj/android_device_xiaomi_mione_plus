12eb35501a1936e674b68cfd270b4d2bda73dc18
BambooIV <ningdisheng@hotmail.com>
2017-04-30 23:15:51 +0800

    mione_plus: Checkout to pyramid media configuration

--- device_mione_plus.mk
+++ device_mione_plus.mk
@@ -159,6 +159,10 @@ PRODUCT_COPY_FILES += \
 
 # Media configuration
 PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     device/xiaomi/mione_plus/configs/media_codecs.xml:system/etc/media_codecs.xml \
     device/xiaomi/mione_plus/configs/media_profiles.xml:system/etc/media_profiles.xml
 
